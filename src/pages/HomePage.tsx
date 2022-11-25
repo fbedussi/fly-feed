ae216b1262db806c66b414737aaf5896dcae87eb
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-11-25 17:11:23 +0100

    fix: always use cache

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,15 +6,12 @@ import RightDrawer from '../components/RightDrawer'
 import UpdateButton from '../components/UpdateButton'
 import Main from '../components/Main'
 import { setArticles } from '../state'
-import { FetchOk } from '../model/index'
 import openDb from '../cache'
 
 const HomePage: Component = () => {
   onMount(async () => {
     openDb().then((db: any) => db.readCache().then((cachedDataParsed: any) => {
-      const cachedUpdatedAt = !!cachedDataParsed && new Date(cachedDataParsed.updatedAt)
-      const tenMinutes = 1000 * 60 * 10
-      if (cachedUpdatedAt && cachedUpdatedAt.getTime() - Date.now() < tenMinutes) {
+      if (cachedDataParsed) {
         setArticles(cachedDataParsed.articles)
       }
     }))
