042e1cf2bf51b23c4691838aebf035383c506bde
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-11-25 17:11:40 +0100

    fix: use user as a param

--- src/primitives/useGetSubscriptions.tsx
+++ src/primitives/useGetSubscriptions.tsx
@@ -1,15 +1,11 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { createResource } from 'solid-js';
 import { db } from '../backend/init';
-import { SubscriptionsDb } from '../model';
+import { SubscriptionsDb, User } from '../model';
 import { user } from '../state';
 
-const fetchData = async () => {
-  const userId = user()?.id
-  if (!userId) {
-    throw new Error('missing userId')
-  }
-
+const fetchData = async (user: User) => {
+  const userId = user.id
   const docRef = doc(db, 'subscriptions', userId);
   const querySnapshot = await getDoc(docRef);
   const data = querySnapshot.data() as SubscriptionsDb
