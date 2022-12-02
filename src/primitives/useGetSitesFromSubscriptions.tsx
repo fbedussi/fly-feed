9b162cb519dd6219dddffc9e98b810113ddab674
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-02 13:36:58 +0100

    fix: ignore muted categories

--- src/primitives/useGetSitesFromSubscriptions.tsx
+++ src/primitives/useGetSitesFromSubscriptions.tsx
@@ -1,15 +1,14 @@
-import { SiteDb, SubscriptionsDb } from '../model'
+import {SiteDb, SubscriptionsDb} from '../model'
 
 const addCategoryIdToSite = <S extends SiteDb>(site: S, categoryId: string) => ({
   ...site,
   categoryId,
 })
 
-export const useGetSitesFromSubscriptions = (subscriptions?: SubscriptionsDb) => {
-  const sitesInCategories = subscriptions?.categories
-    .flatMap(({ id, sites }) => sites
+export const useGetSitesFromSubscriptions = (subscriptions?: SubscriptionsDb, excludeMutedCategories = false) => {
+  return subscriptions?.categories
+    .filter(category => excludeMutedCategories ? !category.muted : true)
+    .flatMap(({id, sites}) => sites
       .map(site => addCategoryIdToSite(site, id))
     ) || []
-
-  return sitesInCategories
 }
