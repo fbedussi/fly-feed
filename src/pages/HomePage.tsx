0d494004ebad49179fa7ae5772694864117e047f
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-02 17:56:13 +0100

    chore: formatting

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,41 +1,43 @@
-import {Component, onMount} from 'solid-js'
+import { Component, onMount } from 'solid-js'
 
 import Header from '../components/Header'
 import LeftDrawer from '../components/LeftDrawer'
 import RightDrawer from '../components/RightDrawer'
 import UpdateButton from '../components/UpdateButton'
 import Main from '../components/Main'
-import {setArticles} from '../state'
+import { setArticles } from '../state'
 import openDb from '../cache'
 import EditSiteDrawer from '../components/EditSiteDrawer'
 import EditCategoryDrawer from '../components/EditCategoryDrawer'
 
-import styles from "./HomePage.module.css";
+import styles from './HomePage.module.css'
 
 const HomePage: Component = () => {
   onMount(async () => {
-    openDb().then((db: any) => db.readCache().then((cachedDataParsed: any) => {
-      if (cachedDataParsed) {
-        setArticles(cachedDataParsed.articles)
-      }
-    }))
-  });
+    openDb().then((db: any) =>
+      db.readCache().then((cachedDataParsed: any) => {
+        if (cachedDataParsed) {
+          setArticles(cachedDataParsed.articles)
+        }
+      }),
+    )
+  })
 
   return (
     <>
-      <EditSiteDrawer/>
-      <EditCategoryDrawer/>
+      <EditSiteDrawer />
+      <EditCategoryDrawer />
 
-      <Header/>
+      <Header />
       <div class={styles.menuAndMain}>
-        <LeftDrawer/>
+        <LeftDrawer />
 
-        <Main/>
+        <Main />
       </div>
 
-      <RightDrawer/>
+      <RightDrawer />
 
-      <UpdateButton/>
+      <UpdateButton />
     </>
   )
 }
