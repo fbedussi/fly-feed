23a30ffd62c7969884eb4557dccf79c678c7439a
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-04 17:31:48 +0100

    refactor: better indexed db typing

--- src/cache.ts
+++ src/cache.ts
@@ -4,68 +4,74 @@ if (!('indexedDB' in window)) {
   throw new Error("This browser doesn't support IndexedDB")
 }
 
-let db: any
+let db: IDBDatabase | undefined
 
 const storeName = 'cache'
 
-const openDb = () =>
+const openDb = (): Promise<typeof dbInterface> =>
   new Promise((resolve, reject) => {
     const dbName = 'fly-feed'
     const request = indexedDB.open(dbName)
     let newDb = false
 
-    request.addEventListener('error', (event) => reject(`Error opening DB ${dbName}: ${request.error}`))
+    request.addEventListener('error', () => reject(`Error opening DB ${dbName}: ${request.error}`))
 
-    request.addEventListener('upgradeneeded', (event: any) => {
-      const db = event.target?.result
+    request.addEventListener('upgradeneeded', event => {
+      const db = (event.target as { result: IDBDatabase } | null | undefined)?.result
+
+      if (!db) {
+        throw new Error('not initialized DB')
+      }
 
       try {
         db.createObjectStore(storeName, { keyPath: 'id' })
       } catch (e) {
-        reject('Error creating upadates store')
+        reject('Error creating updates store')
       }
 
       newDb = true
     })
 
-    request.addEventListener('success', (event: any) => {
-      db = event.target.result
+    request.addEventListener('success', event => {
+      db = (event.target as { result: IDBDatabase } | null | undefined)?.result
+      if (!db) {
+        throw new Error('not initialized DB')
+      }
       resolve(dbInterface)
     })
   })
 
 const contentId = '1'
 
-const saveCache = (content: { updatedAt: IsoDate, articles: FetchOk[] }) =>
+const saveCache = (content: { updatedAt: IsoDate; articles: FetchOk[] }): Promise<IDBValidKey> =>
   new Promise((resolve, reject) => {
+    if (!db) {
+      throw new Error('not initialized DB')
+    }
     const transaction = db.transaction([storeName], 'readwrite')
     const request = transaction.objectStore(storeName).put({ ...content, id: contentId })
-    transaction.onerror = () =>
-      reject(`Error writing to ${storeName}: ${transaction.error}`)
+    transaction.onerror = () => reject(`Error writing to ${storeName}: ${transaction.error}`)
 
-    request.onerror = () =>
-      reject(`Error writing to ${storeName}: ${request.error}`)
+    request.onerror = () => reject(`Error writing to ${storeName}: ${request.error}`)
 
     request.onsuccess = () => resolve(request.result) //key
   })
 
-const readCache = () =>
+const readCache = <T>(): Promise<T> =>
   new Promise((resolve, reject) => {
+    if (!db) {
+      throw new Error('not initialized DB')
+    }
     const transaction = db.transaction([storeName])
     const request = transaction.objectStore(storeName).get(contentId)
-    transaction.onerror = () =>
-      reject(`Error reading from ${storeName}: ${transaction.error}`)
+    transaction.onerror = () => reject(`Error reading from ${storeName}: ${transaction.error}`)
 
-    request.onerror = () =>
-      reject(
-        `Error reading from ${storeName}: ${request.error}`,
-      )
+    request.onerror = () => reject(`Error reading from ${storeName}: ${request.error}`)
 
-    request.onsuccess = () => resolve(request.result)
+    request.onsuccess = () => resolve(request.result as T)
   })
 
 const dbInterface = {
-  openDb,
   saveCache,
   readCache,
 }
