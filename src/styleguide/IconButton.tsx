1663c5432aa353c3f64ec81b94509880e576e712
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-28 23:06:32 +0100

    fix: icon button color

--- src/styleguide/IconButton.tsx
+++ src/styleguide/IconButton.tsx
@@ -1,9 +1,10 @@
-import { IconButton as MuiIconButton } from '@suid/material'
-import { IconButtonProps } from '@suid/material/IconButton'
 import { Component } from 'solid-js'
 
+import { IconButton as MuiIconButton } from '@suid/material'
+import { IconButtonProps } from '@suid/material/IconButton'
+
 const IconButton: Component<IconButtonProps> = (props) => {
-  return <MuiIconButton sx={{ color: 'currentColor' }} {...props} />
+  return <MuiIconButton {...props} />
 }
 
 export default IconButton
