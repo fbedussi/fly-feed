45520a63ac1f0745e1d8afa44d9980b450070b8b
Francesco Bedussi <francesco.bedussi@abstract.it>
2022-12-28 23:06:51 +0100

    fix: remove unused right panel items

--- src/components/RightDrawer.tsx
+++ src/components/RightDrawer.tsx
@@ -1,40 +1,28 @@
-import {
-  Component,
-  createSignal
-} from 'solid-js'
+import { Component, createSignal } from 'solid-js'
 
 import { logoutBe } from '../backend/login'
-import {
-  rightDrawerOpen,
-  setRightDrawerOpen, setUser
-} from '../state'
-import {
-  Button, CloseIcon,
-  Drawer, FileDownloadIcon,
-  IconButton,
-  TextField,
-} from '../styleguide'
+import { rightDrawerOpen, setRightDrawerOpen, setUser } from '../state'
+import { Button, CloseIcon, Drawer, FileDownloadIcon, IconButton } from '../styleguide'
 import LogoutModal from './LogoutModal'
 import styles from './RightDrawer.module.css'
 import ImportButton from './ImportButton'
 
-const logout =
-  () =>
-    logoutBe()
-      .then(() => {
-        setUser(null)
-      })
-      .catch(error => {
-        // notificationsActions.addNotification({
-        //   type: 'error',
-        //   messageLabelKey: error.message,
-        //   id: error.message,
-        // }),
-        alert(error.message)
-      })
+const logout = () =>
+  logoutBe()
+    .then(() => {
+      setUser(null)
+    })
+    .catch(error => {
+      // notificationsActions.addNotification({
+      //   type: 'error',
+      //   messageLabelKey: error.message,
+      //   id: error.message,
+      // }),
+      alert(error.message)
+    })
 
 const RightDrawer: Component = () => {
-  const [openLogoutModal, setOpenLogoutModal] = createSignal(false);
+  const [openLogoutModal, setOpenLogoutModal] = createSignal(false)
 
   return (
     <Drawer open={rightDrawerOpen()} anchor="right" onClose={() => setRightDrawerOpen(false)}>
@@ -55,30 +43,23 @@ const RightDrawer: Component = () => {
         <div class={styles.content}>
           <ImportButton />
 
-          <Button
-            variant="outlined"
-            startIcon={<FileDownloadIcon />}
-          >
+          <Button variant="outlined" startIcon={<FileDownloadIcon />}>
             export
           </Button>
 
-          <TextField
-            label="Text preview height (in rows)"
-            type="number"
-            fullWidth
-          />
+          {/*<TextField*/}
+          {/*  label="Consecutive failures before disabling a site"*/}
+          {/*  type="number"*/}
+          {/*  fullWidth*/}
+          {/*/>*/}
 
-          <TextField
-            label="Consecutive failures before disabling a site"
-            type="number"
-            fullWidth
-          />
-
-          <Button variant="text" onClick={() => setOpenLogoutModal(true)}>Logout</Button>
+          <Button variant="text" onClick={() => setOpenLogoutModal(true)}>
+            Logout
+          </Button>
         </div>
       </div>
     </Drawer>
-  );
+  )
 }
 
 export default RightDrawer
