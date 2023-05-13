69209ca3df184db98df253a9ab856bf91dcacaef
Francesco Bedussi <francesco.bedussi@abstract.it>
2023-05-13 16:23:54 +0200

    fix: casing

--- src/components/TopDrawer.tsx
+++ src/components/TopDrawer.tsx
@@ -1,26 +1,24 @@
-import {ParentComponent} from 'solid-js'
+import { ParentComponent } from 'solid-js'
 
-import styles from './topDrawer.module.css'
+import styles from './TopDrawer.module.css'
 
 type Props = {
   open: boolean
   onClose: () => void
 }
 
-const TopDrawer: ParentComponent<Props> = (props) => {
+const TopDrawer: ParentComponent<Props> = props => {
   return (
     <>
-      <div class={[styles.overlay, props.open ? styles.isOpen : ''].join(' ')}
-           onClick={() => props.onClose()}
+      <div
+        class={[styles.overlay, props.open ? styles.isOpen : ''].join(' ')}
+        onClick={() => props.onClose()}
       />
       <div class={[styles.topDrawer, props.open ? styles.isOpen : ''].join(' ')}>
-
-        <div class={styles.topDrawerInner}>
-          {props.children}
-        </div>
+        <div class={styles.topDrawerInner}>{props.children}</div>
       </div>
     </>
-  );
+  )
 }
 
 export default TopDrawer
